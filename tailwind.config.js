9c234d8bab048d3a38e223c1bf02a244fdf2782c
owolabiolutola <oolutolaowolabi@gmail.com>
2022-03-01 18:35:34 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,12 @@
 module.exports = {
+  mode: 'jit',
+
   purge: [
-    './docs/*.html'
+    './docs/*.html',
+    
   ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-   rotate:{'-30':'-30deg',
-    '30':'30deg',
-     '0': '0',
-    '60': '60deg',
-    '90': '90deg',
-    '120': '120deg',
-    '150': '150deg',
-    '180': '180deg',
-    '210': '210deg',
-    '240': '240deg',
-    '270': '270deg',
-    '300': '300deg',
-    '330': '330deg',
-    '360': '360deg',},
-
-    zIndex: {
-      '11': 11,
-      '20': 20,
-      '10': 10,
-      '5': 5,
-      '50': 50,
-    },
-
-    extend: {
-     width:{ '300':'300px', 
-     '3.75':'0.9375rem',
-       },
-      minHeight:{
-   '1/2':'50%',
-      },
-      },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+   theme: {
+     // ...
+   }
+   // ...
+ }
